2180fc1b67c78e9f1e41f2c1ced508f2c1d94f7c
CGM123sherry <g.christinemurithi@gmail.com>
2024-09-01 08:11:29 +0300

    completed the comprehension and generators lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
+# def return_evens(num_list):
 def return_evens(num_list):
-    pass
+    """
+    This function takes a list of numbers and returns a list of even numbers from the input list.
+    """
+    # List comprehension to filter even numbers
+    return [num for num in num_list if num % 2 == 0]
+
+
+
 
 def make_exclamation(sentence_list):
-    pass
+    """
+    This function takes a list of sentences and returns a new list where each sentence
+    ends with an exclamation mark.
+    """
+    # Add an exclamation mark to each sentence in the list
+    return [sentence + "!" for sentence in sentence_list]
